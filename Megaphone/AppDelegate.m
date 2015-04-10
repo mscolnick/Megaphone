70de3fdadb1241834dd936696afda89468eec336
Hriday Kemburu <hridaykemburu@gmail.com>
2015-04-10 15:37:34 -0700

    login added and frameworks synced

--- Megaphone/AppDelegate.m
+++ Megaphone/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
+#import <ParseFacebookUtils/PFFacebookUtils.h>
 
 @interface AppDelegate ()
 
@@ -17,7 +19,40 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    // Override point for customization after application launch.
+    [Parse setApplicationId:@"Vr9N0uG67wnATYRFpxJZsGTsBsGiLr7HofeQ0lAW"
+                  clientKey:@"zVeZXoZQSbnRQNLaHj8H7Zan49lLcOflViOpRIhY"];
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+    // Initialize Parse's Facebook Utilities singleton. This uses the FacebookAppID we specified in our App bundle's plist.
+    [PFFacebookUtils initializeFacebook];
+    
+    self.window = [[UIWindow alloc] initWithFrame:UIScreen.mainScreen.bounds];
+    
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    
+    if ([PFUser currentUser]) {
+        NSLog(@"Logged in");
+        UITabBarController *viewController =[storyboard instantiateViewControllerWithIdentifier:@"tabBarControllerID"];
+        viewController.selectedIndex = 1;
+        self.window.rootViewController = viewController;
+        [self.window makeKeyAndVisible];
+    } else {
+        NSLog(@"Not logged in");
+        UIViewController *viewController=[storyboard instantiateViewControllerWithIdentifier:@"loginViewControllerID"];
+        self.window.rootViewController = viewController;
+        [self.window makeKeyAndVisible];
+    }
+
     return YES;
+}
+
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation {
+    return [FBAppCall handleOpenURL:url
+                  sourceApplication:sourceApplication
+                        withSession:[PFFacebookUtils session]];
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
@@ -36,10 +71,13 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [FBAppCall handleDidBecomeActiveWithSession:[PFFacebookUtils session]];
+    
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    [[PFFacebookUtils session] close];
 }
 
 @end
