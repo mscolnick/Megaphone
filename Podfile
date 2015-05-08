c731f6e4e2236b074631b1d7fe2aead9f5559560
Myles Scolnick <mscolnick@berkeley.edu>
2015-05-08 15:48:30 -0700

    added animations...that was hard. nwo you need to open Megaphone.xcworkspace arther than Megaphone.xcodeproj

---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, "7.0"
+
+target 'Megaphone' do
+
+pod 'Canvas', '~> 0.1.2'
+
+end
+
+target 'MegaphoneTests' do
+
+end
+
---
+++ Pods/Target Support Files/Pods-Megaphone/Pods-Megaphone-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Megaphone : NSObject
+@end
+@implementation PodsDummy_Pods_Megaphone
+@end
